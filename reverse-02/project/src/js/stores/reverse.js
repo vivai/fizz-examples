712746d4e6d36f6aa0503eb7d1c78020787fe45d
Marcus Kruse <kruse@vivai.de>
2016-10-07 12:04:48 +0200

    Updated reverse-02

--- reverse-02/project/src/js/stores/reverse.js
+++ reverse-02/project/src/js/stores/reverse.js
@@ -4,18 +4,24 @@ import {Store} from 'fizz-flux';
 import {reverseStart, reverseRun, reverseStop} from '../actions/reverse';
 import {EMPTY_STRING} from 'jsz-string';
 
-export default Store.create(
+export default Store.create('App',
   {
     content: EMPTY_STRING,
     progress: false
   }
-).register(reverseRun, function(payload) {
-  console.log('store: dispatch reverseRun');
-  this.content = payload.data;
-}).register(reverseStart, function() {
-  console.log('store: dispatch reverseStart');
-  this.progress = true;
-}).register(reverseStop, function() {
-  console.log('store: dispatch reverseStop');
-  this.progress = false;
-});
+).register(reverseRun,
+  function(state, payload) {
+    console.log('store: dispatch reverseRun');
+    return {...state, content: payload.data};
+  }
+).register(reverseStart,
+  function(state) {
+    console.log('store: dispatch reverseStart');
+    return {...state, progress: true};
+  }
+).register(reverseStop,
+  function(state) {
+    console.log('store: dispatch reverseStop');
+    return {...state, progress: false};
+  }
+);
